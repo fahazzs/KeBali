58fdbc9ca9553b2c9da13944d5ae293b5e48fdae
Muhammad Fahaz Ilyasa <98444738+fahazzs@users.noreply.github.com>
2024-06-20 19:41:19 +0700

    Delete app/src/main/java/com/dicoding/kebali/data directory

--- app/src/main/java/com/dicoding/kebali/data/response/TranslationResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.dicoding.kebali.data.response
-
-import com.google.gson.annotations.SerializedName
-
-// Masih bingung
-data class TranslationResponse(
-    @SerializedName("result")
-    val result: String
-)
