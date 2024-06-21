31acbb73edb3884e3b2bf63f8b59cfc2f2fcb825
Muhammad Fahaz Ilyasa <98444738+fahazzs@users.noreply.github.com>
2024-06-21 14:18:42 +0700

    Update MainActivity.kt
    Add SplashScreen

--- app/src/main/java/com/dicoding/kebali/MainActivity.kt
+++ app/src/main/java/com/dicoding/kebali/MainActivity.kt
@@ -11,6 +11,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Thread.sleep(3000)
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
